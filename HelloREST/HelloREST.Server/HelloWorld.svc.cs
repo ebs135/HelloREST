db931d33f010db73d9d35fecab8f3b7586d3a296
Eko B Santoso <ebs135@gmail.com>
2017-09-19 20:01:21 +0700

    First published

---
+++ HelloREST/HelloREST.Server/HelloWorld.svc.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloREST.Server
+{
+	public class HelloWorld : IHelloWorld
+	{
+		public string HaloDunia(string nama)
+		{
+			nama = "Halo dunia, namaku " + nama.ToUpper() + " !!!";
+
+			return nama;
+		}
+	}
+}
---
+++ HelloREST/HelloREST.Server/IHelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace HelloREST.Server
+{
+	[ServiceContract(Namespace = "http://ebs135.my.id/HelloRest")]
+	public interface IHelloWorld
+	{
+		[OperationContract]
+		[WebGet(UriTemplate = "prm?nama={nama}",
+			RequestFormat = WebMessageFormat.Json,
+			ResponseFormat = WebMessageFormat.Json)]
+		string HaloDunia(string nama);
+	}
+}
